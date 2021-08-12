eb353be1805202cf81f42477a5c3de581c5c767d
mymythboy11ggo <41545571+mymythboy11ggo@users.noreply.github.com>
2021-08-12 18:12:12 +0700

    Update InsertDB.py

--- InsertDB.py
+++ InsertDB.py
@@ -5,7 +5,7 @@ import random
 import pymysql
 import re
 conn = pymysql.connect(host='127.0.0.1', unix_socket='/var/run/mysqld/mysqld.sock',
-                        user='root', passwd='danang', db='scraping', charset='utf8')
+                        user='root', passwd='0987410640', db='scraping', charset='utf8')
 cur = conn.cursor()
 cur.execute("USE scraping")
 random.seed(datetime.datetime.now())
