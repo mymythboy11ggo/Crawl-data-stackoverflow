2fb84a2ef9122c99eb125ba82c58cd756a826483
trungkien310599 <73680011+trungkien310599@users.noreply.github.com>
2021-08-12 18:08:07 +0700

    Python file

---
+++ InsertDB.py
@@ -0,0 +1,28 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import datetime
+import random
+import pymysql
+import re
+conn = pymysql.connect(host='127.0.0.1', unix_socket='/var/run/mysqld/mysqld.sock',
+                        user='root', passwd='danang', db='scraping', charset='utf8')
+cur = conn.cursor()
+cur.execute("USE scraping")
+random.seed(datetime.datetime.now())
+def store(question):
+        cur.execute('INSERT INTO question (questtion) VALUES '
+                '("%s")', (question))
+        cur.connection.commit()
+def getLinks():
+        html = urlopen('https://stackoverflow.com/questions')
+        bs = BeautifulSoup(html, 'html.parser')
+        questionAll = bs.find_all('div', {'class':'summary'})
+        for  question in questionAll :
+                question1 = question.find('h3').find('a').get_text()
+                print(question1)
+                store(question1)
+try:
+                links = getLinks()
+finally:
+                cur.close()
+                conn.close()
---
+++ Pagination.py
@@ -0,0 +1,27 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import datetime
+import random
+import pymysql
+import re
+conn = pymysql.connect(host='127.0.0.1', unix_socket='/var/run/mysqld/mysqld.sock',
+                        user='root', passwd='danang', db='scraping', charset='utf8')
+cur = conn.cursor()
+cur.execute("USE scraping")
+random.seed(datetime.datetime.now())
+def query(pageNumber, pageSize):
+        firstRecord = (pageNumber -1)* pageSize
+        maxRecord = pageSize
+        cur.execute('SELECT * FROM question LIMIT '+str(firstRecord)+','+str(maxRecord))
+        result = cur.fetchall()
+        for re in result:
+                print(re)
+try:
+        print("nhap so thu tu trang:")
+        a = int(sys.argv[1])
+        print("nhap so record trong trang")
+        b = int(sys.argv[2])
+        query(a,b)
+finally:
+                cur.close()
+                conn.close()
